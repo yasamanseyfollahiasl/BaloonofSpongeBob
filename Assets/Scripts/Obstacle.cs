cb54fe90d6990d310107ddd5d3b956c0f46c7878
yasamanseyfollahiasl <75227157+yasamanseyfollahiasl@users.noreply.github.com>
2023-04-16 00:31:09 +0430

    complete background + complete spawner

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private float topEdge;
+
+    private void Start() {
+        topEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).y + 2f;
+    }
+
+    public void Update()
+    {
+        transform.position += Vector3.up * GameManager.Instance.gameSpeed * Time.deltaTime;
+
+          
+    }
+}
